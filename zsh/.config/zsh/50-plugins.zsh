ef0d56be69f7ab7713bfff075f5bbd32724aa15e
xwillq <xwillq@pm.me>
2024-09-20 02:04:49 +0500

    Zsh: rewrite and extract termtitle function

--- zsh/.config/zsh/50-plugins.zsh
+++ zsh/.config/zsh/50-plugins.zsh
@@ -14,57 +14,9 @@ zstyle ':fzf-tab:*' single-group prefix color header
 # termtitle
 zstyle ':zim:termtitle' hooks 'precmd' 'preexec'
 zstyle ':zim:termtitle:*' format '%~'
-zstyle ':zim:termtitle:preexec' format '$(termtitle_preexec "$@")'
+autoload -Uz _termtitle
+zstyle ':zim:termtitle:preexec' format '$(_termtitle "$@")'
 
-# Preexec function taken from ohmyzsh/lib/termsupport.zsh
-# TODO: move somewhere
-function termtitle_preexec() {
-    emulate -L zsh
-    setopt extended_glob
-
-    # split command into array of arguments
-    local -a cmdargs
-    cmdargs=("${(z)2}")
-    # if running fg, extract the command from the job description
-    if [[ "${cmdargs[1]}" = fg ]]; then
-        # get the job id from the first argument passed to the fg command
-        local job_id jobspec="${cmdargs[2]#%}"
-        # logic based on jobs arguments:
-        # http://zsh.sourceforge.net/Doc/Release/Jobs-_0026-Signals.html#Jobs
-        # https://www.zsh.org/mla/users/2007/msg00704.html
-        case "$jobspec" in
-            <->) # %number argument:
-                # use the same <number> passed as an argument
-                job_id=${jobspec} ;;
-            ""|%|+) # empty, %% or %+ argument:
-                # use the current job, which appears with a + in $jobstates:
-                # suspended:+:5071=suspended (tty output)
-                job_id=${(k)jobstates[(r)*:+:*]} ;;
-            -) # %- argument:
-                # use the previous job, which appears with a - in $jobstates:
-                # suspended:-:6493=suspended (signal)
-                job_id=${(k)jobstates[(r)*:-:*]} ;;
-            [?]*) # %?string argument:
-                # use $jobtexts to match for a job whose command *contains* <string>
-                job_id=${(k)jobtexts[(r)*${(Q)jobspec}*]} ;;
-            *) # %string argument:
-                # use $jobtexts to match for a job whose command *starts with* <string>
-                job_id=${(k)jobtexts[(r)${(Q)jobspec}*]} ;;
-        esac
-
-        # override preexec function arguments with job command
-        if [[ -n "${jobtexts[$job_id]}" ]]; then
-            1="${jobtexts[$job_id]}"
-            2="${jobtexts[$job_id]}"
-        fi
-    fi
-
-    # cmd name only, or if this is sudo or ssh, the next cmd
-    local CMD="${1[(wr)^(*=*|sudo|ssh|-*)]:gs/%/%%}"
-    local LINE="${2:gs/%/%%}"
-
-    echo "$CMD – %~"
-}
 
 
 #-------------------------------------------------------------------------------
