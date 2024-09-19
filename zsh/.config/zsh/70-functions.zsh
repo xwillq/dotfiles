d9a8903e5606799ef218e1c4aaf58cc3c8a03363
xwillq <xwillq@pm.me>
2024-09-20 02:06:37 +0500

    Zsh: change functions path

--- zsh/.config/zsh/70-functions.zsh
+++ zsh/.config/zsh/70-functions.zsh
@@ -2,8 +2,8 @@
 # Set up FPATH
 #-------------------------------------------------------------------------------
 
-if [[ -d "$XDG_DATA_HOME/zsh/functions:$FPATH" ]]; then
-    FPATH="$XDG_DATA_HOME/zsh/functions:$FPATH"
+if [[ -d "$XDG_DATA_HOME/zsh/site-functions" ]]; then
+    FPATH="$XDG_DATA_HOME/zsh/site-functions:$FPATH"
 fi
 
 
